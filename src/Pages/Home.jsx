ce2aee37c996e301f5714e5235e3c06c31ab0809
krish <sharmageeta847@gmail.com>
2024-05-30 11:25:54 +0530

    footer1 and 2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from '../Components/Navbar'
 import Carousel from '../Components/Carousel'
 import Odop from '../Components/Odop.jsx'
 import CFC from '../Components/CFC.jsx'
+import Footer from '../Components/Footer.jsx'
+import Footer2 from '../Components/Footer2.jsx'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
     <Carousel/>
      <Odop/>
       <CFC/>
+      <Footer/>
+      <Footer2/>
     </>
   )
 }
