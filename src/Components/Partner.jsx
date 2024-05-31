110875a753e482877f041928c9a75dc0171ce3c3
Kritika754 <yadavkritika745@gmail.com>
2024-05-31 15:40:23 +0530

    Partners

--- src/Components/Partner.jsx
+++ src/Components/Partner.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import './Partner.css'
 
 const Partner = () => {
 
@@ -38,17 +39,28 @@ const Partner = () => {
         autoPlaySpeed:2000,
         slidesToShow:6,
         arrows:false,
+        slidesToScroll:6,
 
         responsive:[
             {
                 breakpoint:1250,
                 settings:{
                     slidesToShow:5,
-                    slidesToScroll:1,
+                    slidesToScroll:5,
                     infinite: true,
                 },
 
                 },
+                {
+                  breakpoint:1050,
+                  settings:{
+                      slidesToShow:4,
+                      slidesToScroll:4,
+                      infinite: true,
+                  },
+  
+                  },
+               
             {
                 breakpoint: 576,
                 settings:{
@@ -64,16 +76,16 @@ const Partner = () => {
   return (
     <div className='w-full flex overflow-x-hidden'>
       <div className='w-1/5 flex items-center justify-center '>
-        <h1 className='text-3xl font-semibold border-r-[3px] border-yellow-400 p-2'>Industry Partners</h1>
+        <h1 className='text-3xl text-center font-semibold border-r-[3px] border-yellow-400 p-2'>Industry Partners</h1>
       </div>
 
-   <div className='w-4/5 flex flex-row '>
+   <div className='w-4/5 flex mt-10 '>
       <Slider {...settings} className='w-full'>
          
       {item.map((item, index) => {
     return (
         <div key={index} className=" py-4 px-3">
-            <div className='flex bg-[#ffda4d] text-center items-center justify-center flex h-40 items-center flex-col  rounded-lg'>
+            <div className='flex  text-center font-bold text-black justify-center flex h-32 items-center flex-col  rounded-lg'>
                 
                 <h1>{item.title}</h1>
             </div>
