9f3ff3f9451525726b3f906416d3f58fd1da407d
Kritika754 <yadavkritika745@gmail.com>
2024-06-03 23:35:40 +0530

    responsive

--- src/Pages/Machine.jsx
+++ src/Pages/Machine.jsx
@@ -22,18 +22,19 @@ const jsonData = {
 
 const Machine = () => {
   return (
-    <div className='w-full pt-16 md:pt-20 bg-gray-900 text-white pb-16'>
-      <div className='flex flex-row '>
+    <div className='w-full pt-16 sm:pt-20 bg-gray-900 text-white pb-16'>
+      <div className='flex sm:flex-row flex-col'>
 
-        <div className='w-[30%]'>
+        <div className='sm:w-[30%] w-full'>
              <img src="https://ucarecdn.com/f3c874a3-70b7-44a7-a9ef-cd8460b9ef7d/-/crop/505x649/274,86/-/preview/3000x3000/+" className='' alt="" />
         </div>
-        <div className='w-[70%] px-28 flex flex-col items-center justify-center '>
+        <div className='sm:w-[70%] w-full px-6 sm:px-10 md:px-20 lg:px-28 flex flex-col items-center justify-center '>
             <div className='flex flex-col w-full justify-center items-center '>
-            <h1 className='text-4xl font-bold m-2'> CNC Wire Cut EDM (E Merge S64)</h1>
+            <h1 className='text-xl md:text-3xl lg:text-4xl font-bold mb-2'> CNC Wire Cut EDM (E Merge S64)</h1>
             <hr className="w-[40%] border-t-[3px] border-yellow-300 mb-10" />
             </div>
-                  <p className='text-lg text-justify'>The Wire-Cut EDM eMerge S64 is a cutting-edge precision machining solution, designed for 
+                  <p className='text-base sm:text-md lg:text-lg text-justify'>
+                    The Wire-Cut EDM eMerge S64 is a cutting-edge precision machining solution, designed for 
                     unmatched accuracy and exceptional surface finishes. Key parameters include a workpiece capacity 
                     of 800 x 600 x 300 mm, maximum cutting speed of 300 mm²/min, 
                     wire diameter range of 0.1 to 0.3 mm, and positioning accuracy of ± 0.002 mm.
@@ -43,9 +44,9 @@ const Machine = () => {
         </div>
       </div>
 
-      <div className='flex w-full'>
-      <div className='flex w-[60%] flex-col px-6 py-4 '>
-            <div className='flex items-center pt-6'>
+      <div className='flex sm:flex-row flex-col w-full'>
+      <div className='flex sm:w-[60%] w-full flex-col px-6 py-4 order-2 sm:order-1'>
+            <div className=' items-center pt-6 hidden sm:flex'>
               <p className='border-[#FBDD07] border-[3px] inline-block rounded-lg px-4 py-2 text-lg font-bold'>SPECIFICATIONS</p>
               <hr className="w-[80%] border-t-[3px] border-yellow-300 " />
             </div>
@@ -58,7 +59,11 @@ const Machine = () => {
             </div>
         </div>
 
-      <div className='w-[40%]'>
+      <div className='sm:w-[40%] w-full order-1 sm:order-2'>
+      <div className=' items-center pt-6 flex sm:hidden'>
+              <p className='border-[#FBDD07] border-[3px] inline-block rounded-lg px-4 py-2 text-lg font-bold'>SPECIFICATIONS</p>
+              <hr className="w-[80%] border-t-[3px] border-yellow-300 " />
+            </div>
        <img src="https://ucarecdn.com/0860b2a6-dc4c-417c-a79a-cc2b95b95872/-/crop/559x649/266,52/-/preview/3000x3000/" alt="" />
       </div>
       </div>
