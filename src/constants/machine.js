600aedf5b307c072b58326cccb8e9ccc2b48a222
krish <sharmageeta847@gmail.com>
2024-08-14 14:20:14 +0530

    rnd lab complete

--- src/constants/machine.js
+++ src/constants/machine.js
@@ -493,7 +493,7 @@ export const rndlab = [
     id: 2,
     title: "Post-processing Machines",
     image1: "https://ucarecdn.com/a88ba382-7d52-4b84-9404-bcbc8a35905f/-/preview/1000x772/",
-    image2: "https://ucarecdn.com/c24632c9-fae6-4fe6-9a04-435015008334/",
+    image2: "",
     image: "https://ucarecdn.com/8280ed37-3b8b-4a67-b913-516a2eaa4aa8/-/preview/1000x666/",
     description1:
     {
